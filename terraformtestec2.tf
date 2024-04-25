f9ab1db7ab80c71d01897765965e6c007caaba8b
mani <vadupumani@gmail.com>
2024-04-25 12:55:52 +0530

    this is for practice

---
+++ terraformtestec2.tf
@@ -0,0 +1,29 @@
+# Terraform Configure setting block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" 
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "ec2testserver_terraform" {
+  
+  ami                    = "ami-052cef05d01020f1d"
+  instance_type          = "t2.micro"
+  key_name               = "linux2server"
+  monitoring             = false
+  vpc_security_group_ids = ["sg-08db8e1c598854c12"]
+  subnet_id              = "subnet-c28b7aa9"
+
+  
+}
+
+
